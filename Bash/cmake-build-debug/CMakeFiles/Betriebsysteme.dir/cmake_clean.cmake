95054e263164da87258930b1c9ee996336e23703
Luis D <luis.deutsch@fh-bielefeld.de>
2018-06-09 12:48:21 +0200

    organisation

---
+++ Bash/cmake-build-debug/CMakeFiles/Betriebsysteme.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Betriebsysteme.dir/main.c.o"
+  "Betriebsysteme.pdb"
+  "Betriebsysteme"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Betriebsysteme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
