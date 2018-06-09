7b02b0c3d011aab45c6e7ce30ea4011f1980d2d8
Luis D <luis.deutsch@fh-bielefeld.de>
2018-06-09 15:44:08 +0200

    clion-project

---
+++ Scheduling/cmake-build-debug/CMakeFiles/Scheduling.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scheduling.dir/main.c.obj"
+  "Scheduling.pdb"
+  "Scheduling.exe"
+  "Scheduling.exe.manifest"
+  "Scheduling.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Scheduling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
