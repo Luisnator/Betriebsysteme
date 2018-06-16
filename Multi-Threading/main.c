f8890b597f52ddd66f4c2b1bdba1ff891738d7dd
Luis <luisnator@aol.de>
2018-06-16 21:40:53 +0200

    Some Progress

--- Multi-Threading/main.c
+++ Multi-Threading/main.c
@@ -5,16 +5,19 @@
 #include <pthread.h>
 #include <dirent.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <stdbool.h>
 static Queue queue;
 char test;
+bool fin = 0;
 pthread_mutex_t mut;
-typedef struct Job
+typedef struct job
 {
+    char *path;
     char *content;
     char *directory;
     char *filewithoutext;
-};
+}Job;
 
 
 const char *get_filename_ext(const char *filename)
@@ -23,6 +26,7 @@ const char *get_filename_ext(const char *filename)
     if(!dot || dot == filename) return "";
     return dot +1;
 }
+
 char *removeExt(char* file)
 {
     char *retstr;
@@ -67,12 +71,17 @@ void* fillqueue(void *arg)
 {
     DIR *d;
     struct dirent *dir;
-    d = opendir(".");
+    const char *test = (char *)arg;
+    char buf[1000];
+    chdir(test);
+    d = opendir(getcwd(buf, sizeof(buf)));
+
     if(d)
     {
+        printf("Got here");
         while((dir = readdir(d)) != NULL)
         {
-            struct Job job;
+            Job *data = (Job *) malloc(sizeof(Job));
             char buf[PATH_MAX +1];
             char *path = realpath(dir->d_name,buf);
             char *ext = get_filename_ext(&dir->d_name);
@@ -80,20 +89,54 @@ void* fillqueue(void *arg)
             char *fileWithoutExt = removeExt(dir->d_name);
             if(ext != "compr" && type == 8)
             {
-                job.directory = path;
-                job.filewithoutext = fileWithoutExt;
-                job.content = loadfile(path);
-                printf("%s\n",job.content);
+                data->directory = path;
+                printf("%s\n",path);
+                data->filewithoutext = fileWithoutExt;
+                data->path = "./";
+                data->content = loadfile(path);
+                queue_insert(queue,data);
+                printf("Juhu\n");
             }
         }
     }
+    else
+    {
+        printf("Directory not valid") ;
+    }
 }
-int main() {
+void* compressData(void *arg)
+{
+    while(!queue_empty(queue) && fin == 0)
+    {
+        if(!queue_empty(queue))
+        {
+            Job *data = queue_head(queue);
+            queue_delete(queue);
+            FILE *fp;
+            char newFile[1000];
+            strcpy(newFile,data->path);
+            strcat(newFile,data->filewithoutext);
+            strcat(newFile,".compr");
+            fp = fopen(newFile,"w+");
+            printf("%s\n",data->path);
+            printf("%s\n",newFile);
+            Result *result = compress_string(data->content);
+            fprintf(fp,result->data);
+            free(result);
+            fclose(fp);
+        }
+    }
+}
+int main(int argc, char **argv) {
     queue = queue_create();
+    chdir(argv[0]);
+    char buf[100000];
+    printf("%s\n",getcwd(buf, sizeof(buf)));
     pthread_t handle1;
-    char arg1 = 'a';
-    int suc = pthread_create(&handle1,NULL,fillqueue,&arg1);
+    pthread_t handle2;
+    int suc = pthread_create(&handle1,NULL,fillqueue,argv[1]);
     pthread_join(handle1,NULL);
-
+    int suc2 = pthread_create(&handle2,NULL,compressData,argv[1]);
+    pthread_join(handle2,NULL);
     return 0;
 }
