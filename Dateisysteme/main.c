2e2e35edd46a4f5f460f32946275cf6263e7238d
Luis <luisnator@aol.de>
2018-06-21 20:11:50 +0200

    P07 fertig

--- Dateisysteme/main.c
+++ Dateisysteme/main.c
@@ -6,30 +6,41 @@
 #include <errno.h>
 #include <string.h>
 
-int main() {
+int main(int argc,char* argv[]) {
     //WRITE AND READ FILE
-    const char *path = "allesasasss.txt";
-    const char *newpath = "NEWFILEEEEEEEE.COM";
+    const char *path = argv[1];
+    const char *newpath = argv[2];
     int desc = open(path,O_RDWR | O_APPEND | O_CREAT,S_IRWXU);
     printf("%d\n",desc);
-    perror(errno);
-    char buf[3] = "LEL";
-    char buf2[1];
-    ssize_t size = write(desc,buf, sizeof(buf) / sizeof(char));
+    //perror(errno);
     int count = 0;
     off_t endoffile = lseek(desc,0,SEEK_END);
+    int half = endoffile/2;
+    char first[half];
+    char second[half+1];
+    char complete[endoffile+1];
+    lseek(desc,half,SEEK_SET);
+    ssize_t size2 = read(desc,first, sizeof(first));
+    write(STDOUT_FILENO,first, strlen(first));
+    printf("\n");
     lseek(desc,0,SEEK_SET);
-    while(lseek(desc,0,SEEK_CUR) != endoffile)
+    if(half / 2 == 0)
     {
-        count ++;
-        ssize_t size2 = read(desc,buf2,1);
-      //  printf("%c",buf2[0]);
-     //   printf("%d\n",count);
+        ssize_t size3 = read(desc,second, sizeof(second)-1);
+        write(STDOUT_FILENO,second, strlen(second));
     }
+    else
+    {
+        ssize_t size3 = read(desc,second, sizeof(second)-1);
+        write(STDOUT_FILENO,second, strlen(second));
+    }
+    lseek(desc,0,SEEK_SET);
+    read(desc,complete, sizeof(complete)-1);
+    printf("%s",complete);
 
 
 
-
+    //Parse Filename
     int lengthpath = strlen(path);
     char lasttenchar[10];
     if(lengthpath < 10)
@@ -46,7 +57,6 @@ int main() {
             lasttenchar[i] = path[lengthpath-10+i];
         }
     }
-    printf("%s\n",lasttenchar);
     int endoffset = 0;
     int lengthpath2 = strlen(newpath);
     int fin = 0;
@@ -68,7 +78,6 @@ int main() {
         }
     }
     char compl[21+counter];
-    printf("%d\n",counter);
     int c;
     for(int i = 0; i<=lengthpath2-counter && i<11;i++)
     {
@@ -84,25 +93,20 @@ int main() {
     int ccc = cc;
     for(int i = 0;i<counter;i++)
     {
-        compl[cc+i] = newpath[strlen(newpath)-counter+i];
+        compl[cc+i-1] = newpath[strlen(newpath)-counter+i];
         ccc++;
     }
     compl[ccc] = '\0';
 
 
-    for(int i = 0;i<25;i++)
-    {
-        printf("%c",compl[i]);
-    }
-    printf("\n");
-    printf("%s\n",compl);
-    printf("%d\n",strlen(compl));
-
-
     //CHANGE Name
     //const char *temppath = "temp.temp";
-    int desc2 = open(compl,O_RDWR | O_APPEND | O_CREAT,S_IRWXU);
+    int desc2 = open(compl,O_RDWR | O_CREAT,S_IRWXU);
+    write(desc2,complete, sizeof(complete)-1);
     perror(errno);
+    char complete2[lseek(desc2,0,SEEK_END)+1];
+    read(desc2,complete2,strlen(complete2)-1);
+    write(STDOUT_FILENO,complete2, strlen(complete2)-1);
 
 
 
